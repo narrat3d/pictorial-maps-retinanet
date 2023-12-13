cc725cfac7e72aa34815a47668265dfd80676cb6
sraimund <sraimund@ethz.ch>
2023-12-13 12:25:34 +0100

    filter faster rcnn results based on threshold

--- ship_detection/coco_metrics.py
+++ ship_detection/coco_metrics.py
@@ -3,6 +3,7 @@ import numpy as np
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 from ship_detection import config
+import json
 
 aggregated_results = {}
 
@@ -20,15 +21,26 @@ def calculate_results(coco_ground_truth_path, coco_results_path):
     
     return cocoEval.stats
 
+def filter_coco_results(coco_results_path, coco_filtered_results_path):
+    coco_results = json.load(open(coco_results_path))
+    
+    filtered_coco_results = list(filter(lambda coco_result: coco_result["score"] > 0.3,
+                                 coco_results))
+
+    json.dump(filtered_coco_results, open(coco_filtered_results_path, "w"))
+
     
 if __name__ == '__main__':
-    coco_results_root_folder = config.LOG_FOLDER
+    coco_results_root_folder = config.LOG_FOLDER # r"E:\CNN\logs\faster_rcnn"
     folder_names = os.listdir(coco_results_root_folder)
     
     for folder_name in folder_names:
         coco_results_path = os.path.join(coco_results_root_folder, folder_name, config.COCO_RESULTS_FILE_NAME)
         
         if (os.path.exists(coco_results_path)):
+            # coco_filtered_results_path = coco_results_path.replace(".json", "_filtered.json")
+            # filter_coco_results(coco_results_path, coco_filtered_results_path)
+            # results = calculate_results(config.COCO_GROUND_TRUTH_PATH, coco_filtered_results_path)
             results = calculate_results(config.COCO_GROUND_TRUTH_PATH, coco_results_path)
             
             # http://cocodataset.org/#detection-eval
